29fa6feb8455d27f0e9f63abd7ead1f697ed64ac
Alessio <alessio.d@gmail.com>
2018-01-25 10:14:26 +0100

    Big bang

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Styles from'./style.css';
+
+let styles = Object.assign({}, Styles);
+class EmojiCursor extends React.Component {
+
+  render() {
+    console.log(Styles);
+    return (
+      <div></div>
+    );
+  }
+}
+export default EmojiCursor;
